cd2a09860fc2d5d842addb821a88a5f157e9af8c
Sophia S <spswanberg44@gmail.com>
2022-03-08 11:05:15 -0600

    fixed post request and updated dead code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'dart:core';
@@ -10,6 +11,25 @@ void main() => runApp(JoinLeagueApp());
 class JoinLeagueApp extends StatelessWidget {
   JoinLeagueApp(); //I had to make it not const so I could create these functions
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          textTheme: GoogleFonts.bebasNeueTextTheme(
+        Theme.of(context).textTheme,
+      )),
+      routes: {
+        '/': (context) => JoinLeagueScreen(),
+        '/joinleague': (context) => JoinLeagueScreen(),
+      },
+    );
+  }
+}
+
+class JoinLeagueScreen extends StatelessWidget {
+  JoinLeagueScreen();
+
   final _formKey = GlobalKey<FormBuilderState>();
 
   Future<void> sendData(formData) async {
@@ -19,7 +39,7 @@ class JoinLeagueApp extends StatelessWidget {
           'c7d04b42632847e4bd1a633c4e54a75d', //I think this is the same???
     };
     final response = await http.post(
-        Uri.parse('https://csc494apimgmt.azure-api.net/league'),
+        Uri.parse('https://csc494apimgmt.azure-api.net/league/team'),
         headers: header,
         body: json.encode(formData));
     print('Status code: ${response.statusCode}');
@@ -28,55 +48,157 @@ class JoinLeagueApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        '/': (context) => const JoinLeagueScreen(),
-        '/joinleague': (context) => const JoinLeagueScreen(),
-      },
-      /*home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: FormBuilder(
-            key: _formKey,
+    return new Scaffold(
+      backgroundColor: Color.fromRGBO(01, 19, 36, 20),
+      appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          centerTitle: true,
+          title: const Text('Join a League!')),
+      body: new Column(
+        children: [
+          Expanded(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                FormBuilderTextField(
-        ),
-      ),*/
-    );
-  }
-}
-
-class JoinLeagueScreen extends StatelessWidget {
-  const JoinLeagueScreen();
-  final String leaguename =
-      ""; // change this once we figure out how to get league name from the database
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: Center(
-        child: SizedBox(
-          width: 400,
-          child: Card(
-            child: Text('Join league ' + leaguename + '?'),
+                Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 15, bottom: 20),
+                      child: Center(
+                        child: Text(
+                          'Moolah Master',
+                          style: TextStyle(
+                            fontSize: 60,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(239, 41, 23, 1),
+                            //fontFamily: 'Bebas Neue',
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 0, bottom: 60),
+                      child: Image.asset(
+                        'assets/MoolahMasterLogo2.png',
+                        height: 200,
+                        width: 200,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Center(
+                      child: SizedBox(
+                        width: 350,
+                        child: FormBuilder(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(15),
+                              ),
+                              FormBuilderTextField(
+                                name: 'team', //{'name': Sophia}
+                                cursorColor: Colors.white,
+                                style: TextStyle(
+                                    color: Color.fromRGBO(239, 41, 23, 1),
+                                    fontSize: 20),
+                                decoration: InputDecoration(
+                                  labelText: 'ENTER YOUR TEAM NAME',
+                                  labelStyle: TextStyle(
+                                      color: Colors.white, fontSize: 30),
+                                  enabledBorder: const OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Colors.white, width: 0.0)),
+                                  border: const OutlineInputBorder(),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(15),
+                              ),
+                              FormBuilderTextField(
+                                name: 'owner', //{'email': email@email.com}
+                                style: TextStyle(
+                                    color: Color.fromRGBO(239, 41, 23, 1),
+                                    fontSize: 20),
+                                decoration: InputDecoration(
+                                  labelText: 'ENTER OWNERS EMAIL',
+                                  labelStyle: TextStyle(
+                                      color: Colors.white, fontSize: 30),
+                                  enabledBorder: const OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Colors.white, width: 0.0)),
+                                  border: const OutlineInputBorder(),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(15),
+                              ),
+                              FormBuilderTextField(
+                                name: 'leagueID', //{'email': email@email.com}
+                                style: TextStyle(
+                                    color: Color.fromRGBO(239, 41, 23, 1),
+                                    fontSize: 20),
+                                decoration: InputDecoration(
+                                  labelText: 'ENTER LEAGUE ID',
+                                  labelStyle: TextStyle(
+                                      color: Colors.white, fontSize: 30),
+                                  enabledBorder: const OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Colors.white, width: 0.0)),
+                                  border: const OutlineInputBorder(),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(15),
+                              ),
+                              ButtonTheme(
+                                child: TextButton(
+                                  child: Text(
+                                    'Continue',
+                                    style: TextStyle(
+                                      fontSize: 35.0,
+                                      //fontFamily: 'Bebas Neue',
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  style: TextButton.styleFrom(
+                                    primary: Colors.white,
+                                    backgroundColor:
+                                        Color.fromRGBO(239, 41, 23, 1),
+                                    padding: EdgeInsets.only(
+                                        top: 20,
+                                        bottom: 10,
+                                        right: 35,
+                                        left: 35),
+                                    shape: const RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(5.0)),
+                                    ),
+                                  ),
+                                  onPressed: () async {
+                                    //saving form data
+                                    _formKey.currentState?.save();
+                                    final formData = _formKey
+                                        .currentState?.value; //all form values
+                                    print(formData);
+                                    sendData(formData);
+                                    //Navigator.pushNamed(context, '/sixth');
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Join a Moolah Master League',
-            style: Theme.of(context).textTheme.headline2),
+        ],
       ),
     );
   }
